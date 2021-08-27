638005b3403162c310df8c192c40e60f0dac2c61
Mark Daen <89143952+KeplerOrion31@users.noreply.github.com>
2021-08-27 12:34:02 +0800

    Update and rename 01 Summary Statistics to 01-Summary-Statistics.R

--- 01-Summary-Statistics.R
+++ 01-Summary-Statistics.R
@@ -16,3 +16,18 @@ median(belgium_consumption$consumption)
 mean(usa_consumption$consumption)
 median(usa_consumption$consumption)
 
+Mean vs. median
+
+food_consumption %>%
+  # Filter for rice food category
+  filter(food_category == "rice") %>%
+  # Create histogram of co2_emission
+  ggplot(aes(co2_emission)) +
+    geom_histogram()
+
+food_consumption %>%
+  # Filter for rice food category
+  filter(food_category == "rice") %>% 
+  # Get mean_co2 and median_co2
+  summarise(mean_co2 = mean(co2_emission),
+            median_co2 = median(co2_emission))
